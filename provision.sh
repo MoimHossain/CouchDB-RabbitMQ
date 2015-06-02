f5b99f55a00ce880427f922e55cf3d270f5d8297
Moim Hossain <moim.hossain@exact.com>
2015-06-02 14:50:46 +0200

    added docker

--- provision.sh
+++ provision.sh
@@ -1,3 +1,12 @@
+echo "Installing Docker.."
+sudo apt-get -y update
+sudo apt-get -y install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+sudo update-rc.d docker.io defaults
+
+echo "Installing Couchdb.."
+
 sudo apt-get update -y
 sudo apt-get install couchdb -y
 curl localhost:5984
